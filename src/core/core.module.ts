d2945390fe31266eeaf946ee0f4400f7415d3654
wonjjee <yoteamo7@naver.com>
2025-09-13 14:33:40 +0900

    fix: prod

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -11,25 +11,14 @@ import { ErrorFilter } from './filter/error.filter';
 import { TypeOrmModule } from './database/typeorm/typeorm.module';
 import { LoggerModule } from './logger/logger.module';
 import { CacheModule } from './cache/cache.module';
-import { MongooseModule } from './database/mongoose/mongoose.module';
-import { LogModule } from 'src/modules/log/log.module';
 import { JwtModule } from './jwt/jwt.module';
 import { JwtBlacklistGuard } from './guard/jwtBlacklist.guard';
 import { AccessTokenGuard } from './guard/accessToken.guard';
 import { ClsMiddleware } from 'nestjs-cls';
 import { RequestLoggerMiddleware } from './middleware/requestLogger.middleware';
 import { ClsModule } from './cls/cls.module';
-import { QueueModule } from './queue/queue.module';
 
-const modules = [
-  ConfigModule,
-  LoggerModule,
-  CacheModule,
-  LogModule,
-  JwtModule,
-  ClsModule,
-  QueueModule,
-];
+const modules = [ConfigModule, LoggerModule, CacheModule, JwtModule, ClsModule];
 const providers: ClassProvider[] = [];
 const interceptors: ClassProvider[] = [
   { provide: APP_INTERCEPTOR, useClass: ApiResponseInterceptor },
@@ -50,7 +39,7 @@ const filters: ClassProvider[] = [
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forRoot(), MongooseModule.forRoot(), ...modules],
+  imports: [TypeOrmModule.forRoot(), ...modules],
   providers: [...providers, ...interceptors, ...filters, ...guards],
   exports: [...modules, ...providers],
 })
